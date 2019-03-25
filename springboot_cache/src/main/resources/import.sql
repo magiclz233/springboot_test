6af3cc36e34ce811accfec16a977b11bccea7272
magic <magiclz233@163.com>
2019-03-25 17:19:41 +0800

    springboot cache

---
+++ springboot_cache/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO `user` VALUES ('1', 'ityouknow@126.com', 'smile', '123456', '2018', 'neo');
+INSERT INTO `user` VALUES ('2', 'ityouknow@126.com', 'ityouknow', '123456', '2018'
+, 'keepSmile');
