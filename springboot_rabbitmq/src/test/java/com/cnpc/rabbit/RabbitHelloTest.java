a4b8a468f2e60a71f4531309803b536357e5d592
magic <magiclz233@163.com>
2019-03-31 21:48:30 +0800

    springboot rabbitmq test

--- springboot_rabbitmq/src/test/java/com/cnpc/rabbit/RabbitHelloTest.java
+++ springboot_rabbitmq/src/test/java/com/cnpc/rabbit/RabbitHelloTest.java
@@ -3,9 +3,11 @@ package com.cnpc.rabbit;
 
 import com.cnpc.model.User;
 import com.cnpc.rabbit.entity.UserSender;
+import com.cnpc.rabbit.fanout.FanoutSender;
 import com.cnpc.rabbit.hello.HelloSender;
 import com.cnpc.rabbit.many.MSender1;
 import com.cnpc.rabbit.many.MSender2;
+import com.cnpc.rabbit.topic.TopicSender;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +26,10 @@ public class RabbitHelloTest {
     private MSender2 mSender2;
     @Autowired
     private UserSender userSender;
+    @Autowired
+    private TopicSender topicSender;
+    @Autowired
+    private FanoutSender fanoutSender;
     @Test
     public void hello() throws Exception {
         helloSender.send();
@@ -57,4 +63,30 @@ public class RabbitHelloTest {
 
     }
 
+    @Test
+    public void topicSend1() throws InterruptedException {
+        topicSender.send1();
+        Thread.sleep(1000l);
+
+    }
+
+    @Test
+    public void topicSend2() throws InterruptedException {
+        topicSender.send2();
+        Thread.sleep(1000l);
+
+    }
+
+    @Test
+    public void topicSend() throws InterruptedException {
+        topicSender.send();
+        Thread.sleep(1000l);
+
+    }
+
+    @Test
+    public void fanoutSend() throws InterruptedException {
+        fanoutSender.send();
+        Thread.sleep(10001);
+    }
 }
