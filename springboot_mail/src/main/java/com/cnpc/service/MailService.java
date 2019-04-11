2252b66c1b4f35c92f3892f7d12b22b3502feb55
magic <magiclz233@163.com>
2019-04-11 15:01:36 +0800

    springboot javaMailSender

---
+++ springboot_mail/src/main/java/com/cnpc/service/MailService.java
@@ -0,0 +1,12 @@
+package com.cnpc.service;
+
+public interface MailService {
+    public void sendSimpleMail(String to,String subject,String content);
+
+    public void sendHtmlMail(String to, String subject, String content);
+
+    public void sendAttachmentsMail(String to, String subject, String content, String filePath);
+
+    public void sendInlineResourceMail(String to, String subject, String content, String rscPath, String rscId);
+
+}
